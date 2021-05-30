d4e273c1284f2b1cf6826f3f9afd688fa85e9a8c
ahsel21 <sik63rus@mail.ru>
2021-05-30 18:25:29 +0400

    refactored pages and controllers

--- src/main/java/com/example/chat/controller/RoomsController.java
+++ src/main/java/com/example/chat/controller/RoomsController.java
@@ -3,10 +3,12 @@ package com.example.chat.controller;
 import com.example.chat.domain.Rooms;
 import com.example.chat.service.RoomsService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import java.util.List;
 
+@Controller
 public class RoomsController {
 
     private final RoomsService roomsService;
@@ -19,7 +21,7 @@ public class RoomsController {
     @GetMapping("/rooms")
     public String main(Model model) {
         List<Rooms> rooms = roomsService.findAll();
-        model.addAttribute("room", rooms);
+        model.addAttribute("rooms", rooms);
         return "rooms-list";
     }
 
--- src/main/java/com/example/chat/controller/UsersController.java
+++ src/main/java/com/example/chat/controller/UsersController.java
@@ -1,7 +1,26 @@
 package com.example.chat.controller;
 
+import com.example.chat.domain.Users;
+import com.example.chat.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import java.util.List;
 
 @Controller
 public class UsersController {
+    private final UserService userService;
+    @Autowired
+    public UsersController(UserService userService) {
+        this.userService = userService;
+    }
+
+    @GetMapping("/users")
+    public String main(Model model) {
+        List<Users> users = userService.findAll();
+        model.addAttribute("users", users);
+        return "users-list";
+    }
 }
