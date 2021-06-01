97b545f1597581151b143a78089235aac400ec8d
ahsel21 <sik63rus@mail.ru>
2021-06-01 17:34:10 +0400

    Create mainController

--- src/main/java/com/example/chat/controller/MainController.java
+++ src/main/java/com/example/chat/controller/MainController.java
@@ -1,33 +1,45 @@
 package com.example.chat.controller;
 
-import com.example.chat.domain.Roles;
-import com.example.chat.service.RolesService;
+import com.example.chat.domain.Users;
 import com.example.chat.service.UserService;
+import com.example.chat.service.impl.UserDetailsServiceImpl;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
 @Controller
 public class MainController {
 
-//    private final RolesService rolesService;
-//    @Autowired
-//    public MainController(RolesService rolesService) {
-//        this.rolesService = rolesService;
-//    }
-//
-//    @GetMapping("/r")
-//    public String main(Model model) {
-//        List<Roles> roles = rolesService.findAll();
-//        System.out.println(roles);
-//        model.addAttribute("roless", roles);
-//        return "roles";
-//    }
+    @Autowired
+    private UserDetailsServiceImpl userDetailsService;
+    @Autowired
+    private UserService userService;
 
+
+    @GetMapping("/login")
+    public String main(Model model) {
+        return "login";
+    }
+
+
+    @GetMapping("/registration")
+    public String createUserForm(Users users){
+        return "registration";
+    }
+
+    @PostMapping("/registration")
+    public String create(Users users) {
+        System.out.println(0);
+
+        userDetailsService.save(users);
+
+        System.out.println(1);
+        return "redirect:/login";
+    }
 
 }
