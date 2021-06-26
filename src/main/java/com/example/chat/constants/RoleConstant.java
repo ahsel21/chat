9c01623ff9e463fe8a76ee1a6e768bdb2d891f66
ahsel21 <sik63rus@mail.ru>
2021-06-27 02:03:52 +0400

    Добавление констант ролей

---
+++ src/main/java/com/example/chat/constants/RoleConstant.java
@@ -0,0 +1,14 @@
+package com.example.chat.constants;
+
+import lombok.experimental.UtilityClass;
+
+@UtilityClass
+public class RoleConstant {
+
+    public final static String ROLE_ADMIN = "ROLE_ADMIN";
+
+    public final static String ROLE_MODERATOR = "ROLE_MODERATOR";
+
+    public final static String ROLE_USER = "ROLE_USER";
+
+}
