c52014869cdeb07cc4226a76cd1752715ca89d98
ahsel21 <sik63rus@mail.ru>
2021-06-27 02:03:10 +0400

    Небольшие правки в репозиториях

--- src/main/java/com/example/chat/repositories/RoleRoomUserRepository.java
+++ src/main/java/com/example/chat/repositories/RoleRoomUserRepository.java
@@ -12,7 +12,6 @@ import java.util.List;
 @Repository
 public interface RoleRoomUserRepository extends JpaRepository<RoleRoomUser, Long> {
 
-
     List<RoleRoomUser> findByUser(User user);
 
     List<RoleRoomUser> findAllByUserAndRole(User user, RoomRole role);
